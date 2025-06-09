da4cf623f9da8ef86652e404e0178b86f936ed3a
ijunnn <faizunaziz53@gmail.com>
2025-06-10 00:08:50 +0700

    Update : Fixed Name DB

--- db_ndut_mebel.sql
+++ db_ndut_mebel.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `material_db`
+-- Database: `db_ndut_mebel`
 --
 
 -- --------------------------------------------------------
